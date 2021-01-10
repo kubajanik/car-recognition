289257d0cf7d695877299dbcf0d28dc523644b52
Jakub Janik <jakubjanik00@gmail.com>
2021-01-10 11:41:29 +0100

    Make improvements in History
    Update IndexedDB schema.
    Change some styles.

--- src/containers/History/index.jsx
+++ src/containers/History/index.jsx
@@ -6,14 +6,19 @@ export const History = () => {
   const [cars, setCars] = React.useState([])
 
   React.useEffect(() => {
-    db.history.reverse().toArray()
+    db.history
+      .orderBy('date')
+      .reverse()
+      .limit(20)
+      .toArray()
       .then(cars => setCars(cars))
   }, [])
 
   return (
     <div className={styles.history}>
+      <h2>History</h2>
       {cars.map(car => (
-        <div>
+        <div key={`${car.make} ${car.model}`}>
           <img src={car.image} alt="car" />
         </div>
       ))}
--- src/db.js
+++ src/db.js
@@ -2,8 +2,8 @@ import Dexie from 'dexie'
 
 const db = new Dexie('car-recognition')
 db.version(1).stores({
-  history: '[make+model], image',
-  favorite: '[make+model], image'
+  history: '[make+model], image, date',
+  favorite: '[make+model], image, date'
 })
 
 export default db
