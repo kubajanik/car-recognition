c5f1fb283c782c6ece7a864025bc01cf81dbc0b1
Jakub Janik <jakubjanik00@gmail.com>
2021-01-13 11:18:57 +0100

    Implement simple caching

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,3 +1,25 @@
-self.addEventListener('fetch', () => {
-  
+self.addEventListener('fetch', e => {
+  e.respondWith((async () => {
+    const cachedResponse = await caches.match(e.request)
+    if (cachedResponse) {
+      return cachedResponse
+    }
+
+    const response = await fetch(e.request)
+    const cache = await caches.open('cache')
+
+    cache.put(e.request, response.clone())
+    return response
+  })())
+})
+
+self.addEventListener('install', e => {
+  e.waitUntil((async () => {
+    const cache = await caches.open('cache')
+    await cache.addAll([
+      '/style.css',
+      '/index.html',
+      '/static/js'
+    ])
+  })())
 })
