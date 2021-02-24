d1b8a180cff446422aa487082c1d1021900e965c
Jakub Janik <jakubjanik00@gmail.com>
2021-02-24 15:10:30 +0100

    Update car-info Netlify function

--- functions/car-info.js
+++ functions/car-info.js
@@ -4,16 +4,30 @@ const {get} = require('axios')
 exports.handler = async event => {
   const {make, model} = event.queryStringParameters
   const {data} = await get(`https://www.carsguide.com.au/${make}/${model}`)
-  const dom = new JSDOM(data)
+  const {window} = new JSDOM(data)
   
-  const {src} = dom.window.document.querySelector('.fg-img')
+  const {src} = window.document.querySelector('.fg-img')
+
+  let images = window.document.querySelectorAll('.vs-model--image img[data-src]')
+  images = [...images].map(img => 
+    'https:' + img.dataset.src.replace('vehicle_icon', 'hero_low')
+  )
+
+  let cars = window.document.querySelectorAll('.vs-model--title > :first-child')
+  cars = [...cars].map(name => {
+    const [make, model] = name.textContent.split(' ')
+    return {make, model}
+  })
+
+  const similar = cars.map((car, index) => ({...car, image: images[index]}))
 
   return {
     statusCode: 200,
     body: JSON.stringify({
       make,
       model,
-      image: 'https:' + src
+      image: 'https:' + src,
+      similar
     })
   }
 }
