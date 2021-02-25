451b3db1c3ce75fb024213ba9e1005f529bb020c
Jakub Janik <jakubjanik00@gmail.com>
2021-02-25 09:57:28 +0100

    Create Settings component

---
+++ src/containers/Settings/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Header} from '../../components/Header'
+import {Toggle} from '../../components/Toggle'
+import styles from './style.module.scss'
+
+export const Settings = () => {
+  const [theme, setTheme] = React.useState('light')
+
+  React.useEffect(() => {
+    document.body.dataset.theme = theme
+  }, [theme])
+
+  return (
+    <div className={styles.settings}>
+      <Header>Settings</Header>
+
+      <div>
+        <div>Dark Mode</div>
+        <Toggle on={theme === 'dark'} onChange={value => setTheme(value ? 'dark' : 'light')} />
+      </div>
+    </div>
+  )
+}
