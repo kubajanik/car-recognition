fdd228fda100d47810e297dfdab1ec0bdf6b1018
Jakub Janik <jakubjanik00@gmail.com>
2021-03-01 14:36:22 +0100

    Create SearchBox

--- src/components/CarsGrid/index.jsx
+++ src/components/CarsGrid/index.jsx
@@ -1,20 +1,35 @@
 import React from 'react'
 import {CarItem} from '../CarItem'
 import {Header} from '../Header'
+import {SearchBox} from '../SearchBox'
+import {useSearch} from 'react-use-search'
 import db from '../../db'
 import styles from './style.module.scss'
 
 export const CarsGrid = ({cars, title, removable = false}) => {
+  const [filteredCars, query, handleChange] = useSearch(
+    cars, 
+    ({make, model}, query) => (new RegExp(query, 'i')).test(make + ' ' + model), 
+    {filter: true}
+  )
+
   const unfavorite = async (e, {make, model}) => {
     e.stopPropagation()
     await db.favorite.where({make, model}).delete()
   }
 
   return (
-    <>
-      <Header>{title}</Header>
+    <> 
+      <div className={styles.top}>
+        <SearchBox 
+          value={query} 
+          onChange={handleChange} 
+        />
+        <Header>{title}</Header>
+      </div>
+
       <div className={styles.grid}>
-        {cars.map((car, index) => (
+        {filteredCars.map((car, index) => (
           <CarItem 
             car={car} 
             key={index} 
---
+++ src/components/SearchBox/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {IoIosSearch} from 'react-icons/io'
+import {CgClose} from 'react-icons/cg'
+import styles from './style.module.scss'
+
+export const SearchBox = ({value, onChange}) => {
+  const [isOpen, setIsOpen] = React.useState(false)
+  const inputRef = React.useRef()
+  const Icon = isOpen ? CgClose : IoIosSearch
+
+  return (
+    <div className={styles.search}>
+      <Icon 
+        onClick={() => {
+          setIsOpen(!isOpen)
+          
+          if (!isOpen) {
+            inputRef.current.focus()
+          }
+        }}
+      />
+      <input 
+        placeholder="Search for a car"
+        ref={inputRef}
+        data-is-open={isOpen} 
+        value={value} 
+        onChange={onChange} 
+      />  
+    </div>
+  )
+}
