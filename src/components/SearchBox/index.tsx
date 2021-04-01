c7763ea6da12e6ce391dde8dfab9f20508995197
Jakub Janik <jakubjanik00@gmail.com>
2021-04-01 16:59:33 +0200

    Migrate SearchBox component to TypeScript

--- src/components/SearchBox/index.tsx
+++ src/components/SearchBox/index.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, {ChangeEvent, FC} from 'react'
 import {IoIosSearch as SearchIcon} from 'react-icons/io'
 import {CgClose as CloseIcon} from 'react-icons/cg'
 import {useTranslation} from 'react-multi-lang'
 import styles from './style.module.scss'
 
-export const SearchBox = ({value, onChange}) => {
+interface Props {
+  value: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export const SearchBox: FC<Props> = ({value, onChange}) => {
+  const input = React.useRef<HTMLInputElement>(null)
   const [isOpen, setIsOpen] = React.useState(false)
   const Icon = isOpen ? CloseIcon : SearchIcon
   const t = useTranslation()
@@ -12,13 +18,13 @@ export const SearchBox = ({value, onChange}) => {
   return (
     <div className={styles.search}>
       <Icon 
-        onClick={e => {
+        onClick={() => {
           setIsOpen(!isOpen)
-          
-          e.target.nextElementSibling.focus()
+          input.current?.focus()
         }}
       />
       <input 
+        ref={input}
         placeholder={t('cars-grid.search')}
         data-is-open={isOpen} 
         onBlur={e => {
